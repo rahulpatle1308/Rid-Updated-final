3ea0362eac83b601bf7504f6778949d78892e5d1
Deepakkumarsah25 <deepak232a@gmail.com>
2026-05-05 03:53:56 +1000

    add

--- routes/nationalTestSeries/nationalTest.js
+++ routes/nationalTestSeries/nationalTest.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Test = require("../../models/teacherTestModel");
 const User = require("../../models/user"); // 🔥 ADD THIS
-const Teacher = require("../../models/teacher"); // 🔥 ADD THIS
+const Teacher = require("../../models/Teacher"); // 🔥 ADD THIS
 router.get("/", async (req, res) => {
   try {
     const tests = await Test.find({ status: "published" })
